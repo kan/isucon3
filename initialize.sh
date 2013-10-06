58c6116cd8dca9bc863263ac6f4b990cb17ccd22
Tatsuro Hisamori <medianetworks@gmail.com>
2013-10-06 16:34:38 +0900

    added warming up script

--- initialize.sh
+++ initialize.sh
@@ -4,3 +4,4 @@ mysql -uisucon isucon -e 'select * from users' > /dev/null;
 mysql -uisucon isucon -e 'alter table memos add index up(user,is_private)' > /dev/null;
 mysql -uisucon isucon -e 'alter table memos add index created_at_idx(created_at)' > /dev/null;
 mysql -uisucon isucon -e 'alter table memos add index recent(created_at)' > /dev/null;
+cat warmup_url.txt | xargs -P5 -t -n1 /usr/bin/curl > /dev/null
